9b89bf4e7aa9c41ec0ed6c162b5d4b8eacb39e68
yoichi ojima <yoichiojima@gmail.com>
2025-09-28 00:04:29 +0900

    fix: simplify spark session initialisation

--- dbx_bundle/src/dbx_bundle/__init__.py
+++ dbx_bundle/src/dbx_bundle/__init__.py
@@ -1,42 +1,16 @@
 import os
-import sys
-from pathlib import Path
 from pyspark.sql import SparkSession
 from databricks.connect import DatabricksSession
-from databricks.sdk.runtime import spark as dbx_spark
 
 
-def connect_remote_spark() -> SparkSession:
-    try:
-        from dotenv import load_dotenv
-
-        dotenv = Path(__file__).parent.parent.parent / ".env"
-        load_dotenv(dotenv)
-        print(f"loaded .env: {dotenv}", file=sys.stderr)
-    except ImportError:
-        pass
-
-    builder = DatabricksSession.builder
-
-    if os.getenv("DATABRICKS_SERVERLESS", "").lower() == "true":
-        print("connecting serverless clusters", file=sys.stderr)
-        builder = builder.serverless(True)
-    elif cluster_id := os.getenv("DATABRICKS_CLUSTER_ID"):
-        print(f"connecting {cluster_id}", file=sys.stderr)
-        builder = builder.clusterId(cluster_id)
-    else:
-        raise ValueError(
-            "neither DATABRICKS_SERVERLESS / DATABRICKS_CLUSTER_ID are not set properly"
-        )
-
-    return builder.getOrCreate()
+os.environ["DATABRICKS_SERVERLESS_COMPUTE_ID"] = "auto"
 
 
 def spark_session() -> SparkSession:
-    if dbx_spark is not None:
-        return dbx_spark
+    if hasattr(DatabricksSession.builder, "validateSession"):
+        return DatabricksSession.builder.validateSession().getOrCreate()
     else:
-        return connect_remote_spark()
+        return DatabricksSession.builder.getOrCreate()
 
 
 spark = spark_session()
